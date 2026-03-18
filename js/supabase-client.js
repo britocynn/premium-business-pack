7252442f84e050be7f8df3d94d8390873cba4810
britocynn <brito.x6@gmail.com>
2026-03-18 14:55:34 +0000

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = "https://gcbmecsdmnabvhjwsjhx.supabase.co";
+const SUPABASE_KEY = "sb_publishable_y9uOEa58uhu_iP4m5r5okA_iE33XsQQ";
 
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
